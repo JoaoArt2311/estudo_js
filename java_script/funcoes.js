025539db95168fb1675a63e1d3267a93c158c0c5
João Athur M Nunes <joaoarthurmendesnunes8@gmail.com>
2025-10-13 19:16:54 -0300

    Create funcoes.js

---
+++ java_script/funcoes.js
@@ -0,0 +1,23 @@
+//function saudacao(nome){
+//    console.log('Olá,',nome)
+
+//}
+
+const saudacao = (nome) => {
+    console.log('Vida longa  e prospera,', nome)
+}
+
+saudacao('João')
+saudacao('Jessica')
+
+//function calcularDobro (numero) {
+    //return numero * 2
+//}
+
+const calcularDobro = (numero) =>{
+    return numero * 2
+}
+
+const numeroDobrado = calcularDobro(4)
+
+console.log('O dobro de 4 e: ', numeroDobrado)
