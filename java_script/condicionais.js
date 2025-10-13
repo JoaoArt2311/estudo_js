097eaecb00390166ae3918aba41107d08dd34433
João Athur M Nunes <joaoarthurmendesnunes8@gmail.com>
2025-10-13 19:16:37 -0300

    Create condicionais.js

---
+++ java_script/condicionais.js
@@ -0,0 +1,19 @@
+const idade = 17
+
+if(idade >= 18){
+    console.log('Maior de idade')
+}
+else{
+    console.log('Menor de idade')
+}
+//--------------------------------
+const notaDoAluno = 9
+
+if(notaDoAluno >= 9){
+    console.log('Nota excelente')
+} else if (notaDoAluno >= 7){
+    console.log('Nota boa')
+}
+//---------------------------------
+
+idade >= 18? console.log('Maior de Idade') : console.log('Menor de idade')
