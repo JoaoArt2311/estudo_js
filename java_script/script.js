0e4db73cae0c0ed38c00e37961389b4f42debe72
João Athur M Nunes <joaoarthurmendesnunes8@gmail.com>
2025-10-13 19:17:18 -0300

    Create script.js

---
+++ java_script/script.js
@@ -0,0 +1,11 @@
+var idade = '18 anos'
+
+console.log('Minha idade atual: ', idade)
+
+let idade2 = '19 anos'
+
+console.log('A minha idade apos meu aniversario: ', idade2)
+
+const dataAniversario = '23 novembro'
+
+console.log('Meu aniversario e',dataAniversario)
