a053c8b69f34ca8b3f48d650bb9e531fe0e789dc
João Athur M Nunes <joaoarthurmendesnunes8@gmail.com>
2025-10-13 19:16:45 -0300

    Create enquanto.js

---
+++ java_script/enquanto.js
@@ -0,0 +1,15 @@
+for(let Numero = 1; Numero <= 10; Numero++){
+    console.log('Numero Atual:', Numero)
+}
+
+for(let num = 0; num <= 10; num++){
+    if(num % 2 == 0){
+        console.log('Numeros Pares: ', num)
+    }
+}
+
+for(let num1 = 0; num1 <= 10; num1++){
+    if(num1 % 2 != 0){
+        console.log('Numeros Impares:', num1)
+    }
+}
