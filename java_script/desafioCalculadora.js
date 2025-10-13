367c0f53e8147b52f1db4ba7faf4013008aa77a2
João Athur M Nunes <joaoarthurmendesnunes8@gmail.com>
2025-10-13 19:16:42 -0300

    Create desafioCalculadora.js

---
+++ java_script/desafioCalculadora.js
@@ -0,0 +1,7 @@
+console.log('Olá seja bem vindo a calculadora de troco')
+
+let valorCompra = 27.50
+let valorPago = 50
+let troco = valorPago - valorCompra 
+
+console.log('O valor do troco: ', troco,'R$')
