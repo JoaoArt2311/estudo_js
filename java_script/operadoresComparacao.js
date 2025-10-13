840f2a692fb04cc1cc8d0aa50435489e3570dba9
João Athur M Nunes <joaoarthurmendesnunes8@gmail.com>
2025-10-13 19:17:15 -0300

    Create operadoresComparacao.js

---
+++ java_script/operadoresComparacao.js
@@ -0,0 +1,17 @@
+const idadeViny = 36
+const minhaIdade = 18
+
+//const saoIguais = idadeViny == minhaIdade
+
+const comparacao = idadeViny > minhaIdade
+
+console.log('São iguais? ', comparacao)
+
+const idade = 36
+const idadeIrmão = 16
+
+const souMaiordeIdade = idade >= 18
+const irmaoMaiordeIdade = idadeIrmão >= 18
+
+console.log('Sou maior de idade?', souMaiordeIdade)
+console.log('Meu irmão e maior de idade', irmaoMaiordeIdade)
