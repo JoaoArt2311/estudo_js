80b55a40893d221a0ff9421cf33bdeb1bbdef5ab
João Athur M Nunes <joaoarthurmendesnunes8@gmail.com>
2025-10-13 19:17:21 -0300

    Create tipos.js

---
+++ java_script/tipos.js
@@ -0,0 +1,23 @@
+let nome = 'João'
+let idade = 18
+let altura = 1.67
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof altura)
+
+let estaChovendo = false
+
+console.log(typeof estaChovendo)
+
+let cidade 
+
+console.log(typeof cidade)
+
+let aprendeuJS = null
+
+console.log(typeof aprendeuJS)
+
+let simbulo = Symbol()
+
+console.log(typeof simbulo)
