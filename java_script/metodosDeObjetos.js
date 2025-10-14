fab749af0380e087dce258c8a256855d35332a73
João Athur M Nunes <joaoarthurmendesnunes8@gmail.com>
2025-10-13 21:22:59 -0300

    Create metodosDeObjetos.js

---
+++ java_script/metodosDeObjetos.js
@@ -0,0 +1,16 @@
+const pessoa = {
+    nome: 'João',
+    idade: 37,
+    pets: ['Luke'],
+    nacionalidade: 'Brasileiro'
+}
+
+
+const chaves = Object.keys(pessoa)
+const  valores = Object.values(pessoa)
+
+const entradas = Object.entries(pessoa)
+
+console.log('Chaves:', chaves)
+console.log('Valores:', chaves)
+console.log('Entradas:', chaves)
